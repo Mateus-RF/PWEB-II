3ccc374cc220bb4fca13de8f767f4b30a4583c48
Mateus <mateus999391343@gmail.com>
2025-12-14 22:14:15 -0300

    Aula 02/12

---
+++ Exemplos/ex-12-02/app.js
@@ -0,0 +1,12 @@
+import {somar, PI} from "./utils.js"
+
+let valor = 3050;
+let valor2 = 162;
+
+let valorSoma = somar(valor, valor2).parseFloat
+let valorFInal = valorSoma * PI
+
+
+console.log(valorFInal)
+
+
---
+++ Exemplos/ex-12-02/utils.js
@@ -0,0 +1,6 @@
+export function somar(w, b){
+    return w + b;
+
+}
+
+export const PI = 3.1415
