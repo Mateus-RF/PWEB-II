9d1553deba63d687c1b4bfda18b69ed03972014d
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2026-02-10 23:45:33 -0300

    feat: adiciona navegação para cadastro e detalhes do veículo

--- Praticas-Framework/react-app-01/src/App.jsx
+++ Praticas-Framework/react-app-01/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import "./App.css";
@@ -6,51 +6,51 @@ import "./App.css";
 import Card from "./components/Card";
 import SearchBar from "./components/SearchBar";
 
-							
-
-
 const App = () => {
   const [cars, setCars] = useState([]);
   const [allCars, setAllCars] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/anuncios")
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
           throw new Error("Erro na requisição");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         setCars(data);
         setAllCars(data);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, []);
 
   const handleSearch = (query) => {
-    if (query === '') {
+    if (query === "") {
       setCars(allCars);
       return;
     }
 
     setCars(
-      allCars.filter(car =>
-        car.model.toLowerCase().includes(query.toLowerCase())
-      )
+      allCars.filter((car) =>
+        car.model.toLowerCase().includes(query.toLowerCase()),
+      ),
     );
+  };
+  const navigate = useNavigate();
 
-  }
+  const handleCarClick = (car) => {
+    navigate(`/detalhe/${car.id}`);
+  };
 
   return (
     <>
       <h1>Anúncios Recentes</h1>
       <SearchBar handleSearch={handleSearch} />
+      <button className="btn-primary" onClick={() => navigate("/cadastro")}>Cadastrar anúncio</button>
 
       <div className="cards">
-        {cars && cars.map(car => (
-          <Card key={car.id} car={car} />
-        ))}
+        {cars && cars.map((car) => <Card key={car.id} car={car} onClick={() => handleCarClick(car)}/>)}
       </div>
     </>
   );
