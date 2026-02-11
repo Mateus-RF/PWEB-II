49e75cec2d54a5a6cf63f05843f7ae780939def6
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2026-02-10 23:48:45 -0300

    feat(router): adiciona configuração de rotas no main

--- Praticas-Framework/react-app-01/src/main.jsx
+++ Praticas-Framework/react-app-01/src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CadastroVeiculo from "./pages/Formcadastro";
+import DetailsCar from "./pages/detailsCar";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/cadastro" element={<CadastroVeiculo />} />
+        <Route path="/detalhe/:id" element={<DetailsCar />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
