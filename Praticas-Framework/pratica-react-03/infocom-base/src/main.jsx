95917f8f949a33706fa6caed705dc3d7feee6a26
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2026-03-10 21:59:12 -0300

    feat: registrar rota /cart no roteador

---
+++ Praticas-Framework/pratica-react-03/infocom-base/src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import { CartProvider } from "./contexts/CartContext";
+import Cart from "./pages/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  </StrictMode>
+);
+
